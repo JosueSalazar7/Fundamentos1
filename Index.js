6b894afbed51f71c3d8b6a45a8ae296013ebca7b
JosueSalazar7 <josue.salazar@epn.edu.ec>
2023-05-30 17:48:31 -0500

    clase 7

--- Index.js
+++ Index.js
@@ -11,24 +11,78 @@
 // })
 // server.listen(3000)
 // console.log("Servidor OK");
+// const{engine} = require("Express-handlebars")
 const  express = require("express")
-const{engine} = require("Express-handlebars")
-
 const app = express()
-app.engine('.hbs',engine({
-    extname: '.hbs'
-}))
-app.set('view engine',".hbs")
-app.set('views','./src/views')
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(`ruta invocada: ${req.path} - método: ${req.method}`);
+    next()//En midllewares siempre poner el next para que no se quede cargando
+})
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.get('/profile',(req,res)=>{
+    res.send("Bienvenido al perfil del usuario")
+})
+
+app.listen(3000)
+console.log('Servidor ejecutandose en el puerto 3000');
+// app.get("/producto/:id",(req,res)=>{
+//     if(req.params.id === 1){
+//         res.send("Producto 1")
+//     }else if(req.params.id===2){
+//         res.send("Producto 2")
+//     }
+//     res.end()
+// })
+// app.get('/search',(req,res)=>{
+//     console.log(req.query)
+//     if(req.query.data === "js")
+//     {
+//         res.send("Libros de JS")
+//     }
+//     else{
+//         res.send("Libros de dom")
+//     }
 
 
+// app.get('/user/:profile/photo',(req,res)=>{
+// if(req.paramas.profile === "josue"){
+//     res.sendFile("./descarga.jpg"),{root: __dirname}
+// }else{
+//     res.send("No es valido")
+// }
+// })
 
 
+//app.get('/operacion/:num1/:num2',(req,res)=>{
+//     const{num1,num2} =req.params
+//     res.send(`La suma es ${Number(num1)+Number(num2)}`)
+// })
 
+
+//app.post('/productos',(req,res)=>{
+//     // console.log(body)
+//     const {nombre,edad} = req.body
+//     res.send(`Los datos enviados son: ${nombre} ${edad}`)
+// })
+// app.engine('.hbs',engine({
+//     extname: '.hbs'
+// }))
+// app.set('view engine',".hbs")
+// app.set('views','./src/views')
+
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+// const patch=require('patch')
+// app.set('src',patch.join(__dirname,'src'))
+
+// app.get("/home",(req,res)=>{
+//     res.render("home")
+// })
+// app.get("/login",(req,res)=>{
+//     res.render("login")
+// })
 
 
 
@@ -61,5 +115,3 @@ app.get('/', (req, res) => {
 // app.use((req,res)=>{
 //     res.status(404).send("Página no encontrada - 404")
 // })
-app.listen(3000)
-console.log('Servidor ejecutandose en el puerto 3000');
